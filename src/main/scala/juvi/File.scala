edec74125a32a22fad7d425809bfabac7ea8ebcf
Jukka Villstedt <jukka.villstedt@iki.fi>
2011-01-17 20:51:23 +0200

    x

---
+++ src/main/scala/juvi/File.scala
@@ -0,0 +1,25 @@
+package juvi
+
+import java.io.{File, FileInputStream, ByteArrayOutputStream}
+
+object File
+{
+  def fileToString(file: File, encoding: String) = {
+    val inStream = new FileInputStream(file)
+    val outStream = new ByteArrayOutputStream
+    try {
+      var reading = true
+      while ( reading ) {
+        inStream.read() match {
+          case -1 => reading = false
+          case c => outStream.write(c)
+        }
+      }
+      outStream.flush()
+    }
+    finally {
+      inStream.close()
+    }
+    new String(outStream.toByteArray(), encoding)
+  }
+}
